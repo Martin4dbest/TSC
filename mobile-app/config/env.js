62a2426212b99b9bd41c9ea8e9e6c4aa5de05fa0
Martin Agoha <martin4dtruth@gmail.com>
2026-05-24 04:57:32 +0100

    updated

--- mobile-app/config/env.js
+++ mobile-app/config/env.js
@@ -1,12 +1,7 @@
 import { Platform } from "react-native";
 
-const LOCAL_IP = "10.66.220.196";
+const DEV_API = "https://tsc-backend-nefz.onrender.com/api/v1";
 
-const DEV_API =
-  Platform.OS === "android"
-    ? `http://${LOCAL_IP}:8000/api/v1`
-    : `http://${LOCAL_IP}:8000/api/v1`;
-
-const PROD_API = "https://your-production-domain.com/api/v1";
+const PROD_API = "https://tsc-backend-nefz.onrender.com/api/v1";
 
 export const BASE_URL = __DEV__ ? DEV_API : PROD_API;
